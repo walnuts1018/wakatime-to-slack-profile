96bc00d8a90b08ef67fdecb57aa68a81bada0180
walnuts1018 <r.juglans.1018@gmail.com>
2023-09-23 11:13:38 +0900

    fix

--- usecase/emoji.go
+++ usecase/emoji.go
@@ -8,9 +8,9 @@ import (
 
 func (u *Usecase) SetUserCustomStatus(language string) error {
 	if language == "" {
-		err := u.slackClient.SetUserCustomStatus("sloth")
+		err := u.slackClient.SetUserCustomStatus("namakemono")
 		if err == nil {
-			slog.Info("set user custom status", "emoji", "🦥")
+			slog.Info("set user custom status", "emoji", "🦥", "language", language)
 			return nil
 		}
 	}
@@ -19,7 +19,7 @@ func (u *Usecase) SetUserCustomStatus(language string) error {
 	if ok {
 		err := u.slackClient.SetUserCustomStatus(override)
 		if err == nil {
-			slog.Info("set user custom status", "emoji", override)
+			slog.Info("set user custom status", "emoji", override, "language", language)
 			return nil
 		}
 	}
@@ -37,9 +37,9 @@ func (u *Usecase) SetUserCustomStatus(language string) error {
 
 	err = u.slackClient.SetUserCustomStatus("question")
 	if err == nil {
-		slog.Info("set user custom status", "emoji", "❓")
+		slog.Info("set user custom status", "emoji", "❓", "language", language)
 		return nil
 	}
 
-	return fmt.Errorf("failed to find emoji: %v", language)
+	return fmt.Errorf("failed to set emoji: %v", language)
 }
